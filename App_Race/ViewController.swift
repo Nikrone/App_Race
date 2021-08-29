02bf123836c885c467cc839d8cff388dbcccaf98
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-29 10:46:05 +0300

    Initial Commit

---
+++ App_Race/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Race
+//
+//  Created by Evgeniy Nosko on 29.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
